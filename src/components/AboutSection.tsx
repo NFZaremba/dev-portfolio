686296cb351f21af93937172ea9ed5867ed35429
Nick <nfzaremba@gmail.com>
2021-01-06 20:21:26 -0800

    added about page

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,34 @@
+import home1 from "../img/home1.png";
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>Passionate software developer</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              who <span>loves</span>
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>expanding personal growth.</h2>
+          </div>
+        </div>
+        <p>
+          Supportive, enthusiastic team player and leader looking for an
+          environment where I may grow and contribute my professional skills and
+          passion in engineering to the group’s objective.
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <div className="image">
+        <img src={home1} alt="guy with camera" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
