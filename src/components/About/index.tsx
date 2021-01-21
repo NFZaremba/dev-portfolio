a43d0fae7fab9e86c3bc8079c4b6c5822058c8bd
Nick <nfzaremba@gmail.com>
2021-01-20 17:30:43 -0800

    animated about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,6 +3,8 @@ import { CommonDescription, Image, Hide } from "../../shared/CommonStyles";
 // Styles
 import { motion } from "framer-motion";
 import { AboutSection } from "./Styles";
+// Animation
+import { titleAnim, fade, photoAnim } from "../../shared/animation";
 // Iamges
 import home1 from "../../assets/img/home1.png";
 
@@ -12,26 +14,33 @@ const About: React.FC = () => {
       <CommonDescription data-testid="about-section">
         <motion.div>
           <Hide>
-            <motion.h2>Passionate software developer</motion.h2>
+            <motion.h2 variants={titleAnim}>
+              Passionate software developer
+            </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               who <span>loves</span> expanding
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>personal growth.</motion.h2>
+            <motion.h2 variants={titleAnim}>personal growth.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Supportive, enthusiastic team player and leader looking for an
           environment where I may grow and contribute my professional skills and
           passion in engineering to the group’s objective.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </CommonDescription>
       <Image>
-        <img src={home1} alt="guy with camera" data-testid="about-image" />
+        <motion.img
+          variants={photoAnim}
+          src={home1}
+          alt="guy with camera"
+          data-testid="about-image"
+        />
       </Image>
     </AboutSection>
   );
--- src/shared/animation.ts
+++ src/shared/animation.ts
@@ -1,3 +1,8 @@
+/**
+ Propogation 
+If a motion component has children, changes in variant will flow down through the component hierarchy. These changes in variant will flow down until a child component defines its own animate property.
+ */
+
 export const pageAnimation = {
   hidden: {
     opacity: 0,
@@ -8,13 +13,52 @@ export const pageAnimation = {
     y: 0,
     transition: {
       duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 1,
+      duration: 0.5,
+    },
+  },
+};
+
+export const titleAnim = {
+  hidden: { y: 300 },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fade = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
+  },
+};
+
+export const photoAnim = {
+  hidden: {
+    scale: 1.5,
+    opacity: 0,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
     },
   },
 };
